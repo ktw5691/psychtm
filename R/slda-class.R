7d31a0ba78e2afd9890276e27c004f86fd662f3c
ktw5691 <ktw5691@users.noreply.github.com>
2019-01-13 21:53:30 -0500

    Documentation improvements

--- R/slda-class.R
+++ R/slda-class.R
@@ -1,10 +1,16 @@
 #' An S4 class to represent a LDA model.
 #'
-#' @slot beta A K x V x M numeric array of draws of topic-word probabilities
+#' @slot ntopics The number of topics for the LDA model (default: 2).
+#' @slot ndocs The number of documents in the corpus.
+#' @slot nvocab The number of terms in the corpus vocabulary.
+#' @slot nchain The number of iterations of the Gibbs sampler.
+#' @slot beta A K x V x M numeric array of draws of topic-word probabilities.
 #' @slot theta A D x K x M numeric array of draws of document-topic
-#' probabilities
-#' @slot alpha A numeric prior hyperparameter for theta
-#' @slot gamma A numeric prior hyperparameter for beta
+#' probabilities.
+#' @slot alpha A numeric prior hyperparameter for theta.
+#' @slot gamma A numeric prior hyperparameter for beta.
+#' @slot loglike The log-likelihood (up to an additive constant).
+#' @slot logpost The log-posterior (up to an additive constant).
 Lda <- setClass("Lda",
                  slots = list(ntopics   = "numeric",
                               ndocs     = "numeric",
@@ -19,18 +25,25 @@ Lda <- setClass("Lda",
 
 #' An S4 class to represent a sLDA model.
 #'
+#' @inheritParams Lda
 #' @slot eta A M x K numeric matrix of draws of topic regression coefficients
 #' @slot sigma2 A M x 1 numeric vector of draws of residual variance
-#' @slot beta A K x V x M numeric array of draws of topic-word probabilities
-#' @slot theta A D x K x M numeric array of draws of document-topic
-#' probabilities
 #' @slot mu0 A K x 1 numeric matrix of prior means for eta
 #' @slot sigma0 A K x K numeric prior covariance matrix for eta
-#' @slot alpha A numeric prior hyperparameter for theta
-#' @slot gamma A numeric prior hyperparameter for beta
 #' @slot a0 A numeric prior shape hyperparameter for sigma2
 #' @slot b0 A numeric prior rate hyperparameter for sigma2
 #' @slot eta_start A K x 1 numeric matrix of starting values for eta
+#' @slot ntopics The number of topics for the LDA model (default: 2).
+#' @slot ndocs The number of documents in the corpus.
+#' @slot nvocab The number of terms in the corpus vocabulary.
+#' @slot nchain The number of iterations of the Gibbs sampler.
+#' @slot beta A K x V x M numeric array of draws of topic-word probabilities.
+#' @slot theta A D x K x M numeric array of draws of document-topic
+#' probabilities.
+#' @slot alpha A numeric prior hyperparameter for theta.
+#' @slot gamma A numeric prior hyperparameter for beta.
+#' @slot loglike The log-likelihood (up to an additive constant).
+#' @slot logpost The log-posterior (up to an additive constant).
 Slda <- setClass("Slda",
   slots = list(eta       = "matrix",
                sigma2    = "matrix",
@@ -44,6 +57,7 @@ Slda <- setClass("Slda",
 setMethod("initialize", "Lda",
           function(.Object, alpha = 0.1, gamma = 1.01, a0 = 0.001, b0 = 0.001) {
             .Object <- callNextMethod(.Object)
+            .Object@ntopics <- 2
             .Object@alpha <- alpha
             .Object@gamma <- gamma
             .Object@loglike = NaN
