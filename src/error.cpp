ca75d94c0e200ab3fb8b616e3747b43812f9e22c
Kenneth Tyler Wilcox <kwilcox3@nd.edu>
2020-01-26 14:32:32 -0500

    Delete unnecessary error() C++ function

--- src/error.cpp
+++
@@ -1,6 +0,0 @@
-#include <Rcpp.h>
-
-// Function to handle errors
-void error(std::string s) {
-  throw std::runtime_error(s);
-}
--- src/error.h
+++
@@ -1,6 +0,0 @@
-#ifndef ERROR_H
-#define ERROR_H
-
-void error(std::string s);
-
-#endif
