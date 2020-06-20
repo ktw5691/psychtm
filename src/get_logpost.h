5eff4ea330dabfe93d10bc18d23c2e126878ec7e
Kenneth Tyler Wilcox <kwilcox3@nd.edu>
2020-06-20 00:19:25 -0400

    Remove unused arguments from headers

--- src/get_logpost.h
+++ src/get_logpost.h
@@ -12,21 +12,18 @@ double get_lpost_mlr(double ll,
                      double a0, double b0);
 
 double get_lpost_lda(double ll, const arma::mat& theta, const arma::mat& beta,
-                     double gamma_, double alpha_,
-                     uint32_t V, const Rcpp::IntegerVector& docs_index);
+                     double gamma_, double alpha_);
 
 double get_lpost_slda_norm(double ll, const arma::colvec& eta, double sigma2,
                            const arma::mat& theta, const arma::mat& beta,
                            const arma::colvec& mu0, const arma::mat& sigma0,
                            double gamma_, double alpha_,
-                           double a0, double b0, uint32_t V,
-                           const Rcpp::IntegerVector& docs_index);
+                           double a0, double b0);
 
 double get_lpost_slda_logit(double ll, const arma::colvec& eta,
                             const arma::mat& theta, const arma::mat& beta,
                             const arma::colvec& mu0, const arma::mat& sigma0,
-                            double gamma_, double alpha_,
-                            uint32_t V, const Rcpp::IntegerVector& docs_index);
+                            double gamma_, double alpha_);
 
 double eta_logpost_logit(const arma::mat& w, const arma::colvec& y,
                          const arma::vec& eta,
